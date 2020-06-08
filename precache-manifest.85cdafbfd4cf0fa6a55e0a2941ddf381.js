ebf8b0a5ee128d9f11d7b3290ba71c9666054eae
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-06-08 16:16:54 -0400

    Updates

---
+++ precache-manifest.85cdafbfd4cf0fa6a55e0a2941ddf381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955192eb6a675193b1818f60d37f5068",
+    "url": "/color-scheme-generator/index.html"
+  },
+  {
+    "revision": "c2f769f42ceba5cfca09",
+    "url": "/color-scheme-generator/static/css/2.34a15dd8.chunk.css"
+  },
+  {
+    "revision": "94016e07894872fc51b4",
+    "url": "/color-scheme-generator/static/css/main.30285ae8.chunk.css"
+  },
+  {
+    "revision": "c2f769f42ceba5cfca09",
+    "url": "/color-scheme-generator/static/js/2.480b4931.chunk.js"
+  },
+  {
+    "revision": "b89ec519ad42042b552a3d9ce5acde1c",
+    "url": "/color-scheme-generator/static/js/2.480b4931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94016e07894872fc51b4",
+    "url": "/color-scheme-generator/static/js/main.a5860468.chunk.js"
+  },
+  {
+    "revision": "4539536e398fb39228bd",
+    "url": "/color-scheme-generator/static/js/runtime-main.23bff84c.js"
+  }
+]);
